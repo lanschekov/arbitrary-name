09664a9bd6cd1b884fa3dd36e0f5c993c7f6db4e
lanschekov <egorrkaboxer@gmail.com>
2023-11-19 17:41:30 +0300

    Window initially appears without a circle

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 import sys
+from random import randint
 
 from PyQt5 import uic, QtGui
 from PyQt5.QtCore import QPoint
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QMainWindow, QApplication
-from random import randint
 
 
 class Main(QMainWindow):
     def __init__(self):
         super(Main, self).__init__()
+        self.is_draw_circle = False
+
         uic.loadUi("UI.ui", self)
         self.setStyleSheet("""background: transparent;""")
         self.draw_circle_button.clicked.connect(self.draw_circle)
 
     def paintEvent(self, a0: QtGui.QPaintEvent) -> None:
-        draw = QPainter(self)
-        draw.begin(self)
-        draw.setPen(QColor(255, 255, 0))
+        if self.is_draw_circle:
+            draw = QPainter(self)
+            draw.begin(self)
+            draw.setPen(QColor(255, 255, 0))
 
-        w, h = self.width(), self.height()
-        r = randint(1, 100)
-        draw.drawEllipse(QPoint(w // 2, h // 2), r, r)
-        draw.end()
+            w, h = self.width(), self.height()
+            r = randint(1, 100)
+            draw.drawEllipse(QPoint(w // 2, h // 2), r, r)
+            draw.end()
+            self.is_draw_circle = False
 
     def draw_circle(self):
+        self.is_draw_circle = True
         self.update()
 
 
